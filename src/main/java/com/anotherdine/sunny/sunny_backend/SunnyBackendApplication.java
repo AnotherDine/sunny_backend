8229efdff36428b82b95831f09a8a19fb9afff5d
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2024-02-07 12:30:43 +0900

    first commit

---
+++ src/main/java/com/anotherdine/sunny/sunny_backend/SunnyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anotherdine.sunny.sunny_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunnyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anotherdine/sunny/sunny_backend/SunnyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anotherdine.sunny.sunny_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunnyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
