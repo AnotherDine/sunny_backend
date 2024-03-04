dc7a63c2de9bb9827dabdd1ad7678c6341dd5bbe
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2024-03-04 19:59:01 +0900

    feat: CORS allowedHeaders 설정

--- src/main/java/com/anotherdine/sunny/sunny_backend/config/CorsConfig.java
+++ src/main/java/com/anotherdine/sunny/sunny_backend/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*") // 허용할 출처
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowedHeaders("*")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
