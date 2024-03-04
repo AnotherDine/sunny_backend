51d5453a2862b100367a51e73481375c089e4837
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2024-03-04 20:05:33 +0900

    feat: CORS 설정 변경
    @EnableWebMvc 어노테이션 추가, allowedOrigin대신 allowedOriginPatterns("*")로 변경

--- src/main/java/com/anotherdine/sunny/sunny_backend/config/CorsConfig.java
+++ src/main/java/com/anotherdine/sunny/sunny_backend/config/CorsConfig.java
@@ -2,15 +2,17 @@ package com.anotherdine.sunny.sunny_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // 스프링 서버 전역적으로 CORS 설정
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 출처
+                .allowedOriginPatterns("*") // 허용할 출처
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
                 .allowedHeaders("*")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
