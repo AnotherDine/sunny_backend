7e6924cf71b905fd975c81a6332d8777e9b930e7
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2024-03-04 20:09:54 +0900

    feat: CI/CD 설정 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 # start.sh
 
 PROJECT_ROOT="/home/ec2-user/deploy"
-JAR_FILE="$PROJECT_ROOT/server-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/sunny_backend-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -11,7 +11,7 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/build/libs/*.jar $PROJECT_ROOT
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
